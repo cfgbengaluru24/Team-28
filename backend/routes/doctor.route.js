cb03f6ff11a2b8ea4e9490e958a710caee7e63e7
sathvikvittal <sathvik.vittal@gmail.com>
2024-07-27 18:19:20 +0530

    doctor routes

--- backend/routes/doctor.route.js
+++ backend/routes/doctor.route.js
@@ -1,3 +1,9 @@
 const doctorRouter = require('express').Router()
+import { login,register,logout } from '../controllers/doctor.controller';
 
+doctorRouter.post("/register",register)
+doctorRouter.post("/login",login)
+doctorRouter.post("/logout",logout)
+
+doctorRouter.post("/patient_input")
 export default doctorRouter;
