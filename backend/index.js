5d60d3c53f81c1a924d02dcc8b7d8a7374078f6a
piyushs-2004 <piyush712004@gmail.com>
2024-07-27 17:19:35 +0530

    Added .env

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,21 @@ const express = require('express');
 require("dotenv").config();
 const mongoose = require('mongoose');
 const app = express();
+const {errorHandler, notFound}= require("./middleware/errorMiddleware")
 const port = process.env.PORT || 6000;
 
 // Replace with your MongoDB connection string
 const mongoURI =process.env.MONGO_URI;
 
- mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+ mongoose.connect(mongoURI)
    .then(() => console.log('MongoDB connected...'))
+   .then(()=> app.listen(port, () => { 
+    console.log(`Server is running on http://localhost:${port}`);}))
    .catch(err => console.log(err));
+  
+// app.use('/api/user', );
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.use(notFound); 
+app.use(errorHandler);
+
--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const PatientSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        min:2,
-        max:20,
-    },
-    govtId:{
-        type:String,
-        required:true,
-        min:2,
-        max:20,
-        unique: true
-    },
-})
+// const PatientSchema = mongoose.Schema({
+//     name:{
+//         type:String,
+//         required:true,
+//         min:2,
+//         max:20,
+//     },
+//     govtId:{
+//         type:String,
+//         required:true,
+//         min:2,
+//         max:20,
+//         unique: true
+//     },
+// })
