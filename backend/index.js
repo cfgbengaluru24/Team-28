da29daa5c42d4ec75c5fcc542758a8d650a734e9
sathvikvittal <sathvik.vittal@gmail.com>
2024-07-27 17:15:43 +0530

    Authentication for doctors and admins

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 require("dotenv").config();
 const mongoose = require('mongoose');
+
+
+dotenv.config()
 const app = express();
+
+app.use(bodyParser.json({extended:true}))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors({credentials: true ,origin:'http://localhost:3000'}))
+
+
 const port = process.env.PORT || 6000;
 
 // Replace with your MongoDB connection string
